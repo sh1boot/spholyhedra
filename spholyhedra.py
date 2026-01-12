c900bf89c32b336197e57a20d929b3e175cf8be7
S H <3655508+sh1boot@users.noreply.github.com>
2026-01-12 15:19:58 -0800

    refactor

--- spholyhedra.py
+++ spholyhedra.py
@@ -1,70 +1,97 @@
-import argparse
-import math
+#!/bin/env python3
+# Needs: https://pypi.org/project/sdflab/
+# (`pip install sdflab`, not `pip install sdf`)
 from sdf import *
 from sdf.d3 import sdf3, ORIGIN
+import argparse
+import math
 
-def psum(*args, n=1):
-    return pow(sum(map(lambda x: pow(x, n), args)), 1.0 / n)
-
-# TODO: orient all of these consistently.
+def psum(*args, e=1):
+    return sum(map(lambda x: pow(abs(x), e), args))
 
 @sdf3
-def sphube(radius=1, n=4, centre=ORIGIN):
+def tetrasphedron(radius=1, e=4, centre=ORIGIN):
+    dihedral = math.acos(1 / 3)
+    hs = math.sin(dihedral / 2) / 2
+    hc = math.cos(dihedral / 2) / 2
     def f(p):
-        p = abs(p - centre) / radius
+        p = (p - centre) / radius
         px, py, pz = p[:,0], p[:,1], p[:,2]
-        return psum(px, py, pz, n=n) - 1.0
+        x = hs * px
+        y = hc * py
+        z = hc * pz
+        return psum(
+                .5 + x + y,
+                .5 + x - y,
+                .5 - x + z,
+                .5 - x - z,
+                e=e
+        ) - 1.0
     return f
 
 @sdf3
-def sphoctahedron(radius=1, n=6, centre=ORIGIN):
+def sphube(radius=1, e=4, centre=ORIGIN):
+    def f(p):
+        p = (p - centre) / radius
+        px, py, pz = p[:,0], p[:,1], p[:,2]
+        return psum(px, py, pz, e=e) - 1.0
+    return f
+
+def _octasphedron(radius=1, e=6, centre=ORIGIN):
     radius *= math.sqrt(3)
     def f(p):
         p = (p - centre) / radius
         px, py, pz = p[:,0], p[:,1], p[:,2]
-        w = [px + py + pz,
-             px + py - pz,
-             px - py + pz,
-             px - py - pz]
-        return psum(*map(abs,w), n=n) - 1.0
+        return psum(
+                px + py + pz,
+                px + py - pz,
+                px - py + pz,
+                px - py - pz,
+                e=e
+        ) - 1.0
     return f
 
 @sdf3
-def sphodecahedron(radius=1, n=9, centre=ORIGIN):
+def octasphedron(radius=1, e=6, centre=ORIGIN):
+    return _octasphedron(radius=radius, e=e, centre=centre)
+
+@sdf3
+def dodecasphedron(radius=1, e=9, centre=ORIGIN):
     dihedral = math.acos(math.sqrt(5) / -5)
-    c = math.cos(dihedral)
-    s = math.sin(dihedral)
+    c = math.cos(dihedral / 2)
+    s = math.sin(dihedral / 2)
     def f(p):
         p = (p - centre) / radius
         px, py, pz = p[:,0], p[:,1], p[:,2]
-        w = [pz]
-        for i in range(5):
-            x = px * math.cos(math.tau * i / 5)
-            y = py * math.sin(math.tau * i / 5)
-            w.append(s * (x + y) + c * pz)
-        return psum(*map(abs, w), n=n) - 1.0
+        return psum(
+               s * px + c * py,
+               s * px - c * py,
+               s * py + c * pz,
+               s * py - c * pz,
+               s * pz + c * px,
+               s * pz - c * px,
+               e=e
+        ) - 1.0
     return f
 
 @sdf3
-def sphicosahedron(radius=1, n=9, centre=ORIGIN):
-    # TODO: make this right.
+def icosasphedron(radius=1, e=9, centre=ORIGIN):
     dihedral = math.acos(math.sqrt(5) / -3)
-    top = math.acos(math.sqrt((5 - math.sqrt(5)) / 8))
-    side = top + dihedral
-    c0 = math.cos(top)
-    s0 = math.sin(top)
-    c1 = math.cos(side)
-    s1 = math.sin(side)
+    c = math.cos(dihedral / 2)
+    s = math.sin(dihedral / 2)
+    octa = _octasphedron(radius=1, e=e, centre=ORIGIN)
     def f(p):
         p = (p - centre) / radius
         px, py, pz = p[:,0], p[:,1], p[:,2]
-        w = []
-        for i in range(5):
-            x = px * math.cos(math.tau * i / 5)
-            y = py * math.sin(math.tau * i / 5)
-            w.append(c0 * (x + y) + s0 * pz)
-            w.append(c1 * (x + y) + s1 * pz)
-        return psum(*map(abs, w), n=n) - 1.0
+        return psum(
+               s * px + c * py,
+               s * px - c * py,
+               s * py + c * pz,
+               s * py - c * pz,
+               s * pz + c * px,
+               s * pz - c * px,
+               e=e
+        ) + octa(p)
     return f
 
 def main():
@@ -77,17 +104,19 @@ def main():
     parser.add_argument('-o', '--output', type=str, default="out.stl", help="output file name")
     args = parser.parse_args()
     radius = 10
-    bounds = ((-17, -17, -17), (17, 17, 17))
+    bounds = ((-20, -20, -20), (20, 20, 20))
 
     match args.shape.lower():
+        case '4' | 'tetrahedron':
+            solid = tetrasphedron(radius, args.exponent)
         case '6' | 'cube' | 'hexahedron':
             solid = sphube(radius, args.exponent)
         case '8' | 'octahedron':
-            solid = sphoctahedron(radius, args.exponent)
+            solid = octasphedron(radius, args.exponent)
         case '12' | 'dodecahedron':
-            solid = sphodecahedron(radius, args.exponent)
+            solid = dodecasphedron(radius, args.exponent)
         case '20' | 'icosahedron':
-            solid = sphicosahedron(radius, args.exponent)
+            solid = icosasphedron(radius, args.exponent)
         case '_':
             exit(-1)
 
